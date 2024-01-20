b70105830c1b98591553aa5bdf86010634fd8b26
lucaswyl <lucaswongyl@gmail.com>
2024-01-21 04:35:26 +0800

    heroku

--- webpage_to_image.py
+++ webpage_to_image.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import os
 
 def get_image(url):
     # Initialize a Chrome session
     options = webdriver.ChromeOptions()
-    options.headless = True  # Run in headless mode, no UI is displayed
+    options.add_argument("--headless")  # Run in headless mode, no UI is displayed
+    options.add_argument("--disable-gpu")  # Necessary for some versions of Chrome
+    options.add_argument("--no-sandbox")  # Bypass OS security model, required on Heroku
+    options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
     options.add_argument("--window-size=1350,1080")  # Set appropriate window size
     options.add_argument("--hide-scrollbars")  # Hide scrollbars to avoid them appearing in screenshots
     service = Service(ChromeDriverManager().install())
