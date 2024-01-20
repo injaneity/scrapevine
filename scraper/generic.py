2cb8cd398596e35eed1385403aa4d90c3e9feac8
lucaswyl <lucaswongyl@gmail.com>
2024-01-21 04:03:57 +0800

    heroku test

--- scraper/generic.py
+++ scraper/generic.py
@@ -1,47 +1,47 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import re  # Import the regular expressions module
+# import requests
+# from bs4 import BeautifulSoup
+# import pandas as pd
+# import re  # Import the regular expressions module
 
-def scrape_shoes(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        return "Failed to retrieve the website"
+# def scrape_shoes(url):
+#     response = requests.get(url)
+#     if response.status_code != 200:
+#         return "Failed to retrieve the website"
 
-    soup = BeautifulSoup(response.content, 'html.parser')
+#     soup = BeautifulSoup(response.content, 'html.parser')
     
-    # Use regular expression to find all divs containing 'product' in their class
-    product_regex = re.compile('.*product-.*')
-    products = soup.find_all('div', class_=product_regex)
+#     # Use regular expression to find all divs containing 'product' in their class
+#     product_regex = re.compile('.*product-.*')
+#     products = soup.find_all('div', class_=product_regex)
     
-    name_regex = re.compile('.*(title|name|product-name).*')
-    price_regex = re.compile('.*price.*')
+#     name_regex = re.compile('.*(title|name|product-name).*')
+#     price_regex = re.compile('.*price.*')
 
 
-    data = []
-    for product in products:
-        # Extracting product name and price
-        name_tag = product.find('a', class_=name_regex)
-        price_tag = product.find('div', class_=price_regex)
+#     data = []
+#     for product in products:
+#         # Extracting product name and price
+#         name_tag = product.find('a', class_=name_regex)
+#         price_tag = product.find('div', class_=price_regex)
 
-        if name_tag and price_tag:
-            name = name_tag.get_text(strip=True)
-            price = price_tag.get_text(strip=True)
-            data.append({'Product Name': name, 'Price': price})
+#         if name_tag and price_tag:
+#             name = name_tag.get_text(strip=True)
+#             price = price_tag.get_text(strip=True)
+#             data.append({'Product Name': name, 'Price': price})
 
-    df = pd.DataFrame(data)
-    df.drop_duplicates(inplace=True)
-    df.reset_index(drop=True, inplace=True)
+#     df = pd.DataFrame(data)
+#     df.drop_duplicates(inplace=True)
+#     df.reset_index(drop=True, inplace=True)
 
 
-    return df
+#     return df
 
-# Usage example
-url = 'https://www.pazzion.com/collections/shoes-flats'
-df = scrape_shoes(url)
+# # Usage example
+# url = 'https://www.pazzion.com/collections/shoes-flats'
+# df = scrape_shoes(url)
 
-# Output the data
-print(df)
+# # Output the data
+# print(df)
 
-# Optionally, save to an Excel file
-df.to_excel('pazzion_shoes.xlsx', index=False)
+# # Optionally, save to an Excel file
+# df.to_excel('pazzion_shoes.xlsx', index=False)
--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 from webpage_to_image import get_image
 from image_summary import encode_image, summarize_image
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 get_image('https://www.nike.com/sg/t/air-jordan-1-low-shoes-6Q1tFM/553558-161')
 
