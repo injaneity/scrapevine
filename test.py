870d0f9c5399f53d0ca7789469697451e03adaef
injaneity <zanecheejy@gmail.com>
2024-01-22 02:57:34 +0800

    test to find heroku domain

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import os
+print(os.environ.get('HEROKU_APP_DEFAULT_DOMAIN_NAME'))
+
 from webpage_to_image import get_image
 from image_summary import encode_image, summarize_image
 import logging
@@ -10,3 +13,4 @@ logging.basicConfig(
 get_image('https://www.lovebonito.com/sg/mira-knit-midi-dress.html')
 
 print(summarize_image(encode_image('webpage_screenshot.png')))
+
