3aa785502ca36534f5af7ba576c0389805d514ee
lucaswyl <lucaswongyl@gmail.com>
2024-03-20 18:27:26 +0800

    adding flask

---
+++ backend/parsers/flask_app.py
@@ -0,0 +1,54 @@
+from flask import request, jsonify, Flask, g
+from celery import Celery
+import os
+import json
+import redis
+
+redis_conn = redis.from_url(os.getenv("REDIS_URL"))
+
+app = Flask(__name__)
+app.json.sort_keys = False
+
+celery = Celery(__name__)
+# Configure Celery settings
+app.config['CELERY_BROKER_URL'] = os.getenv('CLOUDAMQP_URL')
+app.config['CELERY_RESULT_BACKEND'] = 'rpc://'
+
+@celery.task
+def process_data(url, tags, keywords):
+
+    results = main(url, tags, keywords)
+
+    redis_conn.set('my_key', json.dumps(results))
+    print("This are the results", results)
+
+@app.route('/receive_data', methods=['POST'])
+def receive_data():
+    # Get JSON data sent from the frontend
+    data = request.get_json()
+    
+    # Log or process the data here
+    print("Data received:", data)
+
+    url = data['siteUrl']
+    tags = data['tags']
+    keywords = data['dataRequirements']
+
+    task = process_data.delay(url, tags, keywords)  # Sending the task to the queue
+    return jsonify({"task_id": task.id}), 202
+
+@app.route('/reply_result', methods=['POST'])
+def reply_result():
+    
+    results = redis_conn.get('my_key')
+    print("This are the results again", results)
+
+    if results:
+        return json.loads(results.decode('utf-8'))  # Decode from bytes to string
+    else:
+        return jsonify({"message": "No data available"}), 202
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
