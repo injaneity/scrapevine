2b496c743180b90d869652cb6f68430b42100afc
lucaswyl <lucaswongyl@gmail.com>
2024-04-30 01:56:24 +0800

    pse

--- pse.py
+++ pse.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def product_search(tags, link):
-    
     search_url = "https://www.googleapis.com/customsearch/v1"
-    webpage_urls = []
+    all_webpage_urls = []  # List to hold all found URLs, including duplicates
+    unique_webpage_urls = []  # List to hold unique URLs
     num_results_per_page = 5
     for page_num in range(0, 2):
         start_index = (page_num * num_results_per_page) + 1
@@ -19,25 +19,28 @@ def product_search(tags, link):
             'num': num_results_per_page
         }
         response = requests.get(search_url, params=params)
-        result = ""
         if response.status_code == 200:
             result = response.json()
         else:
-            if response.status_code == 429:
-                print("Error " + str(response.status_code) + ": Rate Limited Reached")
-            else:
-                print("Error " + str(response.status_code) + ": Unknown Issue")    
+            print(f"Error {response.status_code}: {response.reason}")
             return ""
 
         # Extracting webpage URLs where images are found
-        webpage_urls.extend([item['image']['contextLink'] for item in result.get('items', [])])
-        #webpage_urls = list(dict.fromkeys(webpage_urls)) # Remove duplicate links
+        all_webpage_urls.extend([item['image']['contextLink'] for item in result.get('items', [])])
 
-        # Check if there are no more results
-        if 'nextPage' not in result.get('queries', {}):
-            break
-    print("Links found: ", str(webpage_urls))
-    return webpage_urls
+    # Identifying duplicates
+    duplicates = []
+    for url in all_webpage_urls:
+        if url in unique_webpage_urls:
+            duplicates.append(url)
+        else:
+            unique_webpage_urls.append(url)
 
+    # Printing out duplicates and total count of duplicates removed
+    print("DUPLICATE LINKS:\n", duplicates)
+    print("NO OF DUPLICATE: ", len(duplicates))
+    
+    print("UNIQUE LINKS:\n", unique_webpage_urls)
+    print("NO OF UNIQUE: ", len(unique_webpage_urls))
 
-
+    return unique_webpage_urls
